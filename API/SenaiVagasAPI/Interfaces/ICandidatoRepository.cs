046b26760d8532b62684a99a0dc2ea2aca22e275
Hbncod <hbncod@gmail.com>
2020-09-12 19:28:04 -0300

    Criação da interface ICandidatoRepository

---
+++ API/SenaiVagasAPI/Interfaces/ICandidatoRepository.cs
@@ -0,0 +1,18 @@
+﻿using SenaiVagasAPI.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiVagasAPI.Interfaces
+{
+    public interface ICandidatoRepository
+    {
+        void Criar(Candidato candidato);
+        void Atualizar(int idCandidato, Candidato NovosDados);
+        void Deletar(int idCandidato);
+        Candidato BuscarPorId(int idCandidato);
+        List<Candidato> Listar();
+
+    }
+}
