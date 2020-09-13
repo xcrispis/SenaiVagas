94fa7e6885404927076c21f1a4b018a9f2dd790d
Matheus Esteves <matheusesantos@gmail.com>
2020-09-13 13:32:12 -0300

    Criação do endpoint Get pra listar todos os usuários no UsuarioController.

--- API/SenaiVagasAPI/Controllers/UsuarioController.cs
+++ API/SenaiVagasAPI/Controllers/UsuarioController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SenaiVagasAPI.Repositories;
 
 namespace SenaiVagasAPI.Controllers
 {
@@ -14,6 +15,26 @@ namespace SenaiVagasAPI.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        //Criação do objetivo que receberá todos os métodos da interface
+        private UsuarioRepository _usuarioRepository;
 
+        //Instância do objeto para ter referencia de uso no Repository
+        public UsuarioController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_usuarioRepository.Listar());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+            }
+        }
     }
 }
