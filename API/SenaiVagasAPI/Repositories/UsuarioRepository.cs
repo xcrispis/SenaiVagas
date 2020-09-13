43bd93beedd0a47d7322c6b22bf869d6440c442b
Matheus Esteves <matheusesantos@gmail.com>
2020-09-13 13:03:41 -0300

    Criação do UsuarioRepository jutamente com o método de atualizar

---
+++ API/SenaiVagasAPI/Repositories/UsuarioRepository.cs
@@ -0,0 +1,51 @@
+﻿using SenaiVagasAPI.Contexts;
+using SenaiVagasAPI.Domains;
+using SenaiVagasAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiVagasAPI.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        ContextBd ctx = new ContextBd();
+        public void Atualizar(int id, Usuario usuarioAtualizado)
+        {
+            Usuario usuarioBuscado = ctx.Usuario.Find(id);
+
+            if (usuarioBuscado != null)
+                if (usuarioBuscado.Email != null)
+                    usuarioBuscado.Email = usuarioAtualizado.Email;
+                if (usuarioBuscado.Senha != null)
+                    usuarioBuscado.Senha = usuarioAtualizado.Senha;
+                if (usuarioBuscado.FkTipoUsuario != null)
+                    usuarioBuscado.FkTipoUsuario = usuarioAtualizado.FkTipoUsuario;
+
+            ctx.Usuario.Update(usuarioBuscado);
+
+            ctx.SaveChanges();
+        }
+
+        public Usuario BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Usuario novoUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> Listar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
