72dde61fdcbde2d7ae6d5f9918f668e5c8911105
Matheus Esteves <matheusesantos@gmail.com>
2020-09-13 12:57:30 -0300

    Criação da Interface de Usuário

---
+++ API/SenaiVagasAPI/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,21 @@
+﻿using SenaiVagasAPI.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiVagasAPI.Interfaces
+{
+    interface IUsuarioRepository
+    {
+        List<Usuario> Listar();
+
+        Usuario BuscarPorId(int id);
+
+        void Cadastrar(Usuario novoUsuario);
+
+        void Atualizar(int id, Usuario usuarioAtualizado);
+
+        void Deletar(int id);
+    }
+}
