afcb5a1f24f9dbd4d09468982ffb00c003e36d4b
Matheus Esteves <matheusesantos@gmail.com>
2020-09-14 22:33:19 -0300

    Criação da interface da IEmpresaRepository e do repositorie EmpresaRepository com a implementação da interface.

---
+++ API/SenaiVagasAPI/Interfaces/IEmpresaRepository.cs
@@ -0,0 +1,20 @@
+﻿using SenaiVagasAPI.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiVagasAPI.Interfaces
+{
+    interface IEmpresaRepository
+    {
+        List<Empresa> Listar();
+
+        Empresa BuscarPorId(int id);
+        void Cadastrar(Empresa novaEmpresa);
+
+        void Atualizar(int id, Empresa empresaAtualizada);
+
+        void Deletar(int id);
+    }
+}
---
+++ API/SenaiVagasAPI/Repositories/EmpresaRepository.cs
@@ -0,0 +1,40 @@
+﻿using SenaiVagasAPI.Contexts;
+using SenaiVagasAPI.Domains;
+using SenaiVagasAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiVagasAPI.Repositories
+{
+    public class EmpresaRepository : IEmpresaRepository
+    {
+
+        ContextBd ctx = new ContextBd();
+        public void Atualizar(int id, Empresa empresaAtualizada)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Empresa BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Empresa novaEmpresa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Empresa> Listar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
