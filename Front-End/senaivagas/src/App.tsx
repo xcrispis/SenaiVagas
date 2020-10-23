794a7327b950ec81d38ba4d88f9394c816d22897
Marco F. Basile <uyguy66@hotmail.com>
2020-10-23 16:52:36 -0300

    adicionado footer, pastas, e css global

---
+++ Front-End/senaivagas/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Footer />    
+      </div>
+  );
+}
+
+export default App;
