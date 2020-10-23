7401612e467b22dbe48c27afc7d47073b21d5322
Marco F. Basile <uyguy66@hotmail.com>
2020-10-23 17:03:17 -0300

    adicionando footer, css global e font-style global, linkando no header do index da public

--- Front-End/senaivagas/src/App.tsx
+++ Front-End/senaivagas/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-    </div>
+      <Footer />    
+      </div>
   );
 }
 
