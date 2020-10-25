a5709ad2456e1af68a8e1f298332e86bdac29a8b
Marco F. Basile <uyguy66@hotmail.com>
2020-10-24 23:21:28 -0300

    adicionando mudanças, logos e routers

--- Front-End/senaivagas/src/App.tsx
+++ Front-End/senaivagas/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Footer from './components/footer';
+import Routers from './routers';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <Footer />    
+      <Routers/>  
       </div>
   );
 }
---
+++ Front-End/senaivagas/src/pages/vagasGeraisCandidato/index.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import './style.css';
+import '../../assets/styles/global.css';
+import ibmLogo from '../../assets/images/ibmlogo.png';
+
+
+function vagasGerais() {
+    return (
+        <div className="maindiv">
+            <section className="container-geral">
+                <div className="alinha-linha">
+                    <div className="container-individual">
+                        <img src={ibmLogo} alt="" />
+                        <div className="container-2">
+                            <p className="cargo-text">Desenvolvedor(a) Front-End Junior.</p>
+                            <p className="nome-empresa-text"> International Business Machines Corporation</p>
+                        </div>
+
+                        <div className="container-3">
+                            <div className="mini-container">
+                                <p className="local-title">Local</p>
+                                <p className="local-text">Home Office</p>
+                            </div>
+                            <div className="mini-container">
+                                <p className="habilidades-title">Habilidades Requeridas</p>
+                                <p className="habilidades-text">Html, Css, Javascript, React e AngularJS</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="blue-div"></div>
+                </div>
+
+                <div className="alinha-linha">
+                    <div className="container-individual">
+                        <img src={ibmLogo} alt="" />
+                        <div className="container-2">
+                            <p className="cargo-text">Desenvolvedor(a) Front-End Junior.</p>
+                            <p className="nome-empresa-text"> International Business Machines Corporation</p>
+                        </div>
+
+                        <div className="container-3">
+                            <div className="mini-container">
+                                <p className="local-title">Local</p>
+                                <p className="local-text">Home Office</p>
+                            </div>
+                            <div className="mini-container">
+                                <p className="habilidades-title">Habilidades Requeridas</p>
+                                <p className="habilidades-text">Html, Css, Javascript, React e AngularJS</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="blue-div"></div>
+                </div>
+
+
+                <div className="alinha-linha">
+                    <div className="container-individual">
+                        <img src={ibmLogo} alt="" />
+                        <div className="container-2">
+                            <p className="cargo-text">Desenvolvedor(a) Front-End Junior.</p>
+                            <p className="nome-empresa-text"> International Business Machines Corporation</p>
+                        </div>
+
+                        <div className="container-3">
+                            <div className="mini-container">
+                                <p className="local-title">Local</p>
+                                <p className="local-text">Home Office</p>
+                            </div>
+                            <div className="mini-container">
+                                <p className="habilidades-title">Habilidades Requeridas</p>
+                                <p className="habilidades-text">Html, Css, Javascript, React e AngularJS</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="blue-div"></div>
+                </div>
+
+
+                <div className="alinha-linha">
+                    <div className="container-individual">
+                        <img src={ibmLogo} alt="" />
+                        <div className="container-2">
+                            <p className="cargo-text">Desenvolvedor(a) Front-End Junior.</p>
+                            <p className="nome-empresa-text"> International Business Machines Corporation</p>
+                        </div>
+
+                        <div className="container-3">
+                            <div className="mini-container">
+                                <p className="local-title">Local</p>
+                                <p className="local-text">Home Office</p>
+                            </div>
+                            <div className="mini-container">
+                                <p className="habilidades-title">Habilidades Requeridas</p>
+                                <p className="habilidades-text">Html, Css, Javascript, React e AngularJS</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="blue-div"></div>
+                </div>
+
+
+
+            </section>
+        </div>
+    )
+}
+
+export default vagasGerais;
