014ec835cf14b84f6d44a8d397795927dc37c21d
souza-marques <marquesmatheus018@gmail.com>
2020-11-06 08:14:12 -0300

    terminar cadastrovaga

--- Front-End/senaivagas/src/pages/cadastroVaga/index.tsx
+++ Front-End/senaivagas/src/pages/cadastroVaga/index.tsx
@@ -10,12 +10,16 @@ import '../../Assets/styles/global.css';
 
 function cadastroVaga() {
     const [titulo, setTitulo] = useState('');
-    const [email, setEmail] = useState('');
+    const [habilidade, setHabilidade] = useState('');
+    const [descricao, setDescricao] = useState('');
+    const [areavaga, setAreaVaga] = useState('');
   
     const cadastroVaga = () => {
       const cadastroVaga ={
         titulo: titulo,
-        email: email,
+        habilidade: habilidade,
+        descricao : descricao,
+        areavaga : areavaga,
       }
     
   
@@ -33,7 +37,7 @@ function cadastroVaga() {
           localStorage.setItem('token-SenaiVagas', dados.token)
         }
         else{
-          alert('Senha ou e-mail inválido');
+          alert('Insira as informações');
         }
       })
       .catch(erro => console.error(erro))
@@ -46,26 +50,19 @@ function cadastroVaga() {
 
                 <div className="container-all">
                     <div className="centro">
-                        <form>
+                        <form onSubmit={ event => {
+                  event.preventDefault();
+                  cadastroVaga();
+                }}>
                             <h1>Cadastre sua Vaga</h1>
                             <Input type="text" label="Título da Vaga:" name="titulo" placeholder=" Ex: Consultor em segurança da informação " />
                             <br></br>
-                            <Input type="email" label="E-mail para contato:" name="email"  placeholder=" Ex: email@gmail.com "/>
+                            <Input type="text" label="Area da Vaga:" name="areavaga"  placeholder=" Front-End"/>
                             <br></br>
                             <Input type="text" label="Descrição da vaga:" name="descricao"  placeholder=" Ex: Vaga de segurança da informação, em São Paulo"/>
                             <br></br>
-                            <Input type="text" label="Requisítos e habilidades:" name="requisito"  placeholder=" Ex: Experiência em Java " />
-                            <br></br>
-                            <Input type="text" label="Endereço:" name="endereço"  placeholder=" Ex: Rua Costa Aguiar "/>
-                            <br></br>
-                            <label>Área da Vaga</label>
-                            <br />
-                            <select id="permissao">
-                                <option value="" disabled selected>Selecione</option>
-                                <option value="backend">Back-End</option>
-                                <option value="frontend">Front-End</option>
-                                <option value="redes">Redes</option>
-                            </select>
+                            <Input type="text" label="Requisítos e habilidades:" name="habilidade"  placeholder=" Ex: Experiência em Java " />
+                            <br />               
                             <div className="botao">
                                 <Button value="Enviar" />
                             </div>
